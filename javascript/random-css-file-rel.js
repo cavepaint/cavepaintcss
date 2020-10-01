2558b3e3cd5226aad934160e017aa552b8c0e329
xraymachine19 <rexriepe@gmail.com>
2020-10-01 02:02:51 -0700

    rel directory update

---
+++ javascript/random-css-file-rel.js
@@ -0,0 +1,34 @@
+(function() {
+    var random_css_filenumber = Math.floor(Math.random()*100);
+    if (!sessionStorage.getItem('randomCSS')) {
+        sessionStorage.setItem('randomCSS', random_css_filenumber);
+    }
+
+    if (sessionStorage.getItem('randomCSS')) {
+        var stored_css = sessionStorage.getItem('randomCSS');
+        var sheet = document.createElement('link');
+        sheet.rel  = 'stylesheet';
+        sheet.href = '../css/cavepaint_' + stored_css + '.css';
+        if (document.getElementById('download_it')) {
+            var download_button = document.getElementById('download_it');
+            download_button.href = sheet.href;
+        }
+        
+        document.getElementsByTagName('head')[0].appendChild(sheet);
+    }
+})();
+
+function newCSS() {
+    event.preventDefault();
+    var random_css_filenumber = Math.floor(Math.random()*100);
+    sessionStorage.setItem('randomCSS', random_css_filenumber);
+
+    var sheet = document.createElement('link');
+    sheet.rel  = 'stylesheet';
+    sheet.href = '../css/cavepaint_' + random_css_filenumber + '.css';
+    if (document.getElementById('download_it')) {
+        var download_button = document.getElementById('download_it');
+        download_button.href = sheet.href;
+    }
+    document.getElementsByTagName('head')[0].appendChild(sheet);
+}
